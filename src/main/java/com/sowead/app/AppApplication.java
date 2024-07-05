b808cc2f4ef7271e9268d4b493a1339a6f2b7456
RODRIGO WILLY BOHORQUEZ RECUAY <N00284745@upn.pe>
2024-07-05 15:26:23 -0500

    Test Deploy

--- src/main/java/com/sowead/app/AppApplication.java
+++ src/main/java/com/sowead/app/AppApplication.java
@@ -2,6 +2,10 @@ package com.sowead.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AppApplication {
@@ -10,4 +14,17 @@ public class AppApplication {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+	@Configuration
+	public static class Myconfiguration {
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
